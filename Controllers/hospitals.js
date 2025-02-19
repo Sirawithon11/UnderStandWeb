58d91b038679296ab8a819d1bb2b12ac086e3b33
Sirawit Chindam <Sirawithon@gmail.com>
2025-02-19 21:49:48 +0700

    Chapter05

--- Controllers/hospitals.js
+++ Controllers/hospitals.js
@@ -1,36 +1,73 @@
+const Hospital = require('../models/Hospital') ;
+
 // @desc Get all hospitals
 //@route GET /api/v1/hospitals
 //@access public
-exports.getHospitals =(req,res,next) =>{
-    res.status(200).json({success:true , msg:'Get all Hospitals'}) ;
-}
-
+exports.getHospitals = async (req,res,next) =>{
+    try{
+        const hospitals = await Hospital.find() ;
+        res.status(200).json({success:true ,count: hospitals.length , data: hospitals}) ;
+    }catch(err){
+        res.status(400).json({success:false}) ;
+    }
+};
 
 // @desc Get single hospitals
 //@route GET /api/v1/hospitals/:id
 //@access public
-exports.getHospital =(req,res,next) =>{
-    res.status(200).json({success:true , msg:`Get all Hospitals ${req.params.id}`}) ;
-}
+exports.getHospital = async (req,res,next) =>{
+    try{
+        const hospital = await Hospital.findById(req.params.id) ;
+        if(!hospital){
+            return res.status(400).json({success:false}) ;
+        }
+        res.status(200).json({success:true , data: hospital}) ;
+    }catch(err){
+        res.status(400).json({success:false}) ;
+    }
+};
 
 // @desc Create single hospitals
 //@route post /api/v1/hospitals
 //@access public
-exports.postHospital =(req,res,next)=>{
-    res.status(200).json({success:true , msg :'Create new hospitals'}) ;
+exports.postHospital = async (req,res,next) =>{
+    const hospital = await Hospital.create(req.body) ;
+    res.status(201).json({success:true , data : hospital}) ;
+    
 }
 
 // @desc Update single hospitals
 //@route put /api/v1/hospitals/:id
 //@access public
-exports.putHospital =(req,res,next)=>{
-    res.status(200).json({success:true , msg :`Update hospital ${req.params.id}`}) ;
-}
+exports.putHospital = async(req,res,next)=>{
+    try{
+        const hospital = await Hospital.findByIdAndUpdate(req.params.id,req.body,{
+            new:true,
+            runValidators:true
+        });
+        if(!hospital){
+            return res.status(400).json({success:false});
+        }
+        res.status(200).json({success:true , data:hospital});
+    }catch(err){
+        res.status(400).json({success:false}) ;
+    }
+    
+};
 // @desc Delete single hospitals
 //@route delete /api/v1/hospitals/:id
 //@access public
-exports.deleteHospital = (req,res,next)=>{
-    res.status(200).json({success:true , msg :`Delete hospital ${req.params.id}`}) ;
-}
+exports.deleteHospital = async (req,res,next)=>{
+    try{
+        const hospital = await Hospital.findByIdAndDelete(req.params.id);
+        if(!hospital){
+            return res.status(400).json({success:false}) ;
+        }
+
+        res.status(200).json({success:true , data:{}})
+    } catch(err){
+        res.status(400).json({success:false}) ;
+    }
+};
 
 
--- server.js
+++ server.js
@@ -1,13 +1,20 @@
-const express  = require("express") ; // นำเข้า framwork express
+const express=require("express") ; // นำเข้า framwork express
 const dotenv = require("dotenv") ; // นำเข้า dotenv Library
+const connectDB = require('./config/db'); // นำเข้า file จากการเชื่อมต่อ database
 dotenv.config({path:"./config/config.env"}) ; // set ค่า dot env
 const hospitals =  require("./routes/hospitals") ; // routes file
 
+connectDB(); // connect database
+
 
 const app = express() ; // express() เป็นการสร้าง Application express ซึ่งกำหนดให้ app
-
+app.use(express.json());
 app.use('/api/v1/hospitals',hospitals) ;
 
 
 const PORT =  process.env.PORT ; //เรียกใช้ค่าจาก .env โดยใช้ process.esv
-app.listen(PORT, console.log("Server is running  in",process.env.NODE_ENV , "mode on port " , PORT));// ทำงาน เมื่อเซิร์ฟเวอร์เริ่มต้นสำเร็จ
+const server = app.listen(PORT, console.log("Server is running  in",process.env.NODE_ENV , "mode on port " , PORT));// ทำงาน เมื่อเซิร์ฟเวอร์เริ่มต้นสำเร็จ
+process.on('unhandledRejection',(err,promise)=>{
+    console.log(`Error: ${err.message}`);
+    server.close(()=>process.exit(1)) ;
+});
